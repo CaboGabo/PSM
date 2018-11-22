ff9526175f088f1f9970b3a9bea5f9900ac1a050
gabihr <gabo.alejandro.huitron@gmail.com>
2018-11-21 18:49:31 -0600

    FINAL

--- sillas_y_mesas_escolares.php
+++ sillas_y_mesas_escolares.php
@@ -29,7 +29,7 @@
                         if($nom!="DESC") {
                 ?>
                 <a href="producto.php?id=<?php echo $id ?>">
-                <div class="col-sm-2">
+                <div class="col-lg-2">
                     <div class="panel panel-primary">
                         <div class="panel-body tam-min"><img class="img-responsive img-center tam-max" src="<?php echo $foto ?>" alt="<?php echo $nom ?>"/ ></div>
                         <div class="panel-footer tam-des"><?php echo $nom ?></div>
